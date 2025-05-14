081916edeac9f408165e32a7de34a9d10818cc01
Mahmut KELES <mahmutt.keles38@gmail.com>
2025-05-14 10:05:09 +0300

    Servisler oluşturuldu.

---
+++ PavoRestClient/Services1/SatisGiderPusulasiService.cs
@@ -0,0 +1,25 @@
+﻿using PavoDeneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavoRestClient.Services1
+{
+    public class SatisGiderPusulasiService
+    {
+        private static async Task SatisGiderPusulasi(TransactionData transactionData)
+        {
+            var url = "https://192.168.1.203:4567/CompleteSale";
+
+            //TransactionHandle ve Sale nesnesi gönderilmeli
+            //Sale nesnesindeki Orderno alanı satış iptal edilmek istendiği zaman kullanılmalı ve unique olmalı
+            TransactionData payload = transactionData;
+        
+
+            //Apiye istek attığımız yer
+            await ApiPostIstegiService.PostAsync(url, payload);
+        }
+    }
+}
---
+++ PavoRestClient/Services1/SatisIptalService.cs
@@ -0,0 +1,25 @@
+﻿using PavoDeneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavoRestClient.Services1
+{
+    public class SatisIptalService
+    {
+        private static async Task SatisIptal(TransactionData transactionData)
+        {
+            var url = "https://192.168.1.203:4567/CancelSale";
+
+            //TransactionHandle ve Sale nesnesi gönderilmeli
+            //Sale nesnesine Orderno kesin olarak gönderilmeli
+            TransactionData payload = transactionData;
+
+
+            //Apiye istek attığımız yer
+            await ApiPostIstegiService.PostAsync(url, payload);
+        }
+    }
+}
