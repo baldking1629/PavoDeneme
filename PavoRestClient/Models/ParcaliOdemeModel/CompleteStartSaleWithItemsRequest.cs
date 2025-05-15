99e95d4e4136157829c937f2c6630c345ac8d6dc
ömer <omerbaykoca@gmail.com>
2025-05-15 06:05:23 +0300

    parcalı odeme prop eklendi

---
+++ PavoRestClient/Models/ParcaliOdemeModel/CompleteStartSaleWithItemsRequest.cs
@@ -0,0 +1,15 @@
+﻿using PavoDeneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavoRestClient.Models.ParcaliOdemeModel
+{
+    public class CompleteStartSaleWithItemsRequest
+    {
+        TransactionHandle TransactionHandle { get; set; }
+        StartSaleWithItemsSale Sale { get; set; }
+    }
+}
---
+++ PavoRestClient/Models/ParcaliOdemeModel/StartSaleWithItemsSale.cs
@@ -0,0 +1,26 @@
+﻿using PavoDeneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavoRestClient.Models.ParcaliOdemeModel
+{
+    public class StartSaleWithItemsSale
+    {
+        public string RefererApp { get; set; }
+        public string RefererAppVersion { get; set; }
+        public int MainDocumentType { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PriceEffect PriceEffect { get; set; }
+        public bool SendPhoneNotification { get; set; }
+        public bool SendEMailNotification { get; set; }
+        public string NotificationPhone { get; set; }
+        public string NotificationEMail { get; set; }
+        public List<AddedSaleItem> AddedSaleItems { get; set; }
+        public CustomerParty CustomerParty { get; set; }
+        public List<AdditionalInfo> AdditionalInfo { get; set; }
+    }
+}
